ff8cc586b1e12baa49f2d9190e90b19d53739e51
Galaxyben <galaxyben1@gmail.com>
2019-09-04 12:33:23 -0500

    Sriptable objects

---
+++ Assets/NPC/Resources/DialogueNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogo", menuName = "DialogueSystem/DialogueNode", order = 1)]
+public class DialogueNode : ScriptableObject
+{
+    [Tooltip("La oración principal del NPC")]
+    public string sentence;
+    [Tooltip("Nodos con los que se conectarán las respuestas. (Debe ser la misma cantidad de Player Responses")]
+    public DialogueNode[] conections;
+    [Tooltip("Las respuestas posibles del jugador")]
+    public string[] playerResponses;
+}
